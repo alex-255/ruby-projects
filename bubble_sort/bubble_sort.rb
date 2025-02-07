19430053c9d6ce3d2b9877068ed29a2021d527f3
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-02-07 09:34:25 +0100

    bubble sort completed

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,25 @@
+def bubble_sort(array)
+  array_length = array.length
+  counter = 0
+  while counter < array_length
+    array.each_with_index do |element, index|
+      if index != array_length - 1 # if not last element
+        if element > array[index + 1]
+          temp = array[index + 1]
+          array[index + 1] = element
+          array[index] = temp
+        end
+      end
+    end  
+    counter += 1
+  end
+  array
+end
+
+
+
+
+
+
+
+p bubble_sort([4,3,78,2,0,2])
