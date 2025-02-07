3df4da36c6eb1059c8370d0b5a196d299b39844c
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-02-07 01:07:04 +0100

    stock picker project for The Odin Project

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,57 @@
+# Algorithm 2
+# compare results of subtracting
+# linear
+#
+def stock_picker(array)
+  index_of_day_low = 0
+  biggest_difference = 0
+  result = []
+
+  while index_of_day_low < array.length 
+    array.each_with_index do |value, index|
+      if ( value > array[index_of_day_low] &&
+        (value - array[index_of_day_low]) > biggest_difference && 
+        index_of_day_low < index )
+
+        biggest_difference = value - array[index_of_day_low]
+        result = [index_of_day_low, index]
+      end
+    end
+    index_of_day_low += 1
+  end
+  result
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10,20])
+
+
+# 
+#
+# Algorithm 1 (WRONG WAY)
+# to hash
+# sort
+# compare two biggest differ
+# WRONG WAY
+# 
+# def array_to_hash(array)
+#   index = -1
+#   hash = array.to_h do |item| 
+#     index += 1
+#     [index, item]
+#   end
+#   hash
+# end
+
+# hash = array_to_hash([17,3,6,9,15,8,6,1,10])
+# sorted_hash_aka_array = hash.sort_by {|key, value| value }
+# # p sorted_hash_aka_array.to_h
+# counter = 0
+# contr_counter = -1
+# loop do
+#   if sorted_hash_aka_array[counter][0] < sorted_hash_aka_array[contr_counter][0]
+#     [sorted_hash_aka_array[counter][0], sorted_hash_aka_array[contr_counter][0]]
+#     break
+#   else
+#     counter += 1
+#   end  
+# end
